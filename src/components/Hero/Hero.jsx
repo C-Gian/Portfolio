583671e1009cc9326b32e2ea16248babf508b801
C-Gian <gianluca.e.lav@gmail.com>
2023-11-13 18:45:41 +0100

    about

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./Hero.module.css";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Gian</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer with 5 years of experience using react and
+          nodejs. React out if you'd like to learn more!
+        </p>
+        <a className={styles.contactBtn} href="mailto:g.culaon.lav@gmail.com">
+          Contact Me
+        </a>
+      </div>
+      <img
+        className={styles.heroImg}
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero Image"
+      ></img>
+      <div className={styles.topBlur}></div>
+      <div className={styles.bottomBlur}></div>
+    </section>
+  );
+};
